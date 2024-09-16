63189d3be302065e4947350089dcb87af82ba490
Kai Angulo <kangriebling@gmail.com>
2024-09-15 22:44:09 -0700

    Moved CompilationMessage.cs into CompilationResult.cs

--- Source/CompilationResult.cs
+++ Source/CompilationResult.cs
@@ -1,5 +1,56 @@
 namespace DirectXShaderCompiler.NET;
 
+/// <summary>
+/// The severity of the message
+/// </summary>
+public enum MessageSeverity
+{
+    /// <summary>
+    /// Info message (coupled after a warning/error).
+    /// </summary>
+    Info,
+
+    /// <summary>
+    /// Warning message.
+    /// </summary>
+    Warning,
+
+    /// <summary>
+    /// Error message.
+    /// </summary>
+    Error
+}
+
+/// <summary>
+/// Parsed compilation messages
+/// </summary>
+public struct CompilationMessage
+{
+    /// <summary>
+    /// The name of the file the message belongs to. 
+    /// </summary>
+    public string filename;
+
+    /// <summary>
+    /// The line of the message.
+    /// </summary>
+    public int line;
+
+    /// <summary>
+    /// The column or character of the message.
+    /// </summary>
+    public int column;
+
+    /// <summary>
+    /// The severity of the message.
+    /// </summary>
+    public MessageSeverity severity;
+
+    /// <summary>
+    /// The content of the message.
+    /// </summary>
+    public string message;
+}
 
 /// <summary>
 /// The result structure returned by <see cref="ShaderCompiler"/> compilation methods 
